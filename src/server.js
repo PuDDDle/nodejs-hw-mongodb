abf8a5074b38f5d7ce2ee930a98633f9f92dad9b
PuDDDle <volodabelihin35@gmail.com>
2025-06-11 15:36:32 +0300

    first upd

--- src/server.js
+++ src/server.js
@@ -1,68 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
+const express = require('express')
+const contactsController = require('./controllers/contacts')
 
-import { getAllContacts, getContactById } from './services/contacts.js';
-import { getEnvVar } from './utils/getEnvVar.js';
+function setupServer() {
+  const app = express()
 
-const PORT = Number(getEnvVar('PORT', 3000));
+  app.use(express.json())
 
-export function setupServer() {
-  const app = express();
+  app.get('/contacts', contactsController.getAllContacts)
+  app.get('/contacts/:contactId', contactsController.getContactById)
 
-  app.set('json spaces', 2);
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
+  })
 
-  app.use(express.json());
-  app.use(cors());
-
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
-  app.get('/', (req, res) => {
-    res.status(200).json({
-      message: `Server is running, use endpoint '/contacts' and '/contacts/:contactId'`,
-    });
-  });
-
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
+  const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+    console.log(`Server is running on port ${PORT}`)
+  })
+
+  return app
 }
+
+module.exports = setupServer
