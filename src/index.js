8ec688cd954a811c9a5e1077aea2b6f6a77b1c10
PuDDDle <volodabelihin35@gmail.com>
2025-06-11 17:03:40 +0300

    finally

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-require('dotenv').config()
-const setupServer = require('./server')
-const initMongoConnection = require('./db/initMongoConnection')
+import { initMongoDB } from './db/initMongoConnections.js';
+import { setupServer } from './server.js';
 
-console.log('MONGODB_URL:', process.env.MONGODB_URL)
-console.log('MONGODB_USER:', process.env.MONGODB_USER)
-console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD)
-console.log('MONGODB_DB:', process.env.MONGODB_DB)
-;(async () => {
-  await initMongoConnection()
-  setupServer()
-})()
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
+
+bootstrap();
