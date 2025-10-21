063f9f453b5e44638fb3c822791837e8e1eee24d
PuDDDle <volodabelihin35@gmail.com>
2025-10-21 14:55:53 +0300

    udp swagger

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,6 +6,7 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().length(12).pattern(/^\d+$/).required(),
   email: Joi.string().email().allow(null),
+  photo: Joi.any(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .valid(...CONTACT_TYPES)
